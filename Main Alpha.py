90d880875c424982bd0ee1521186801bd1ee33fc
William_Kuang <fuwa165@126.com>
2020-09-22 07:49:14 +0800

    Update the program
    You can easily see the download progress

--- Main Alpha.py
+++ Main Alpha.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import os
 import time
 import requests
@@ -14,9 +15,12 @@ from lxml import etree
 import re
 import socket
 import sys
+from tqdm import tqdm
+import requests#引入requests库
+import time#引入time，计算下载时间
 
 
-# In[2]:
+# In[ ]:
 
 
 print("___    __    _________          ")
@@ -33,7 +37,7 @@ print("----------------------------------")
 print("----------------------------------")
 
 
-# In[3]:
+# In[ ]:
 
 
 #程序声明
@@ -46,20 +50,23 @@ print("作者：KDKDKD！")
 time.sleep(0.1)
 print("部分代码来自于开源社区")
 time.sleep(0.1)
-print("版本 Alpha 0.2.7")
+print("版本 Alpha 0.2.8")
 time.sleep(0.1)
-print("时间：2020-09-20")
+print("时间：2020-09-22")
 time.sleep(0.1)
 print("本软件切勿用于非法用途！")
 time.sleep(0.5)
 print("----------------------------------")
 
 
-# In[7]:
+# In[ ]:
 
 
 #从得到的图片链接下载图片，并保存
+'''
+#作废的代码，应用于0.2.7及之前的程序里
 def SaveImage(link,InputData,count,overtime):
+        
     try: 
         socket.setdefaulttimeout(overtime)
         time.sleep(0.1)
@@ -74,9 +81,51 @@ def SaveImage(link,InputData,count,overtime):
         print("无效链接")
     else:
         print("已有" + str(count) + "张图")
+'''
 
 
-# In[5]:
+# In[ ]:
+
+
+def SaveImage(link,InputData,count,overtime):
+    
+    try:
+        socket.setdefaulttimeout(overtime)
+        time.sleep(0.1)
+        urllib.request.urlretrieve(link,'./'+InputData+'/'+str(count)+'.jpg')
+        print("正在检测网址，请稍后")
+        start = time.time()
+        size = 0
+        path = './'+InputData+'/'+str(count)+'.jpg' #必须是完整路径
+        response = requests.get(link,stream = True)#stream参数设置成True时，它不会立即开始下载，当你使用iter_content或iter_lines遍历内容或访问内容属性时才开始下载
+        chunk_size = 1024#每次块大小为1024
+        content_size = int(response.headers['content-length'])#返回的response的headers中获取文件大小信息
+        print("文件大小："+str(round(float(content_size/chunk_size/1024),4))+"[MiB]")
+        with open(path,'wb') as file:
+            for data in response.iter_content(chunk_size=chunk_size):#每次只获取一个chunk_size大小
+                file.write(data)#每次只写入data大小
+                size = len(data)+size
+                #'r'每次重新从开始输出，end = ""是不换行
+                print('\r'+"【"+str(round(float(size/content_size)*100,2))+"%"+"】"+"已经下载："+int(size/content_size*50)*"█"+" 【"+str(round(size/chunk_size/1024,2))+"MiB】",end="")
+        end = time.time()
+        print("")
+        print("保存图片耗时耗时:"+str(end-start)+"秒")
+        #片段的程序来源：https://blog.csdn.net/YiXiao1997/article/details/86721536?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param
+        
+        print("已有" + str(count) + "张图")
+    except socket.timeout:
+        time.sleep(0.1)
+        print("链接超时")
+    except urllib.error.HTTPError as reason:
+        print(reason)
+    except Exception:
+        time.sleep(0.1)
+        print("无效链接")
+    #else:
+        #print("已有" + str(count) + "张图")
+
+
+# In[ ]:
 
 
 # 主函数
@@ -106,7 +155,7 @@ def main(PageNum,InputData,word,overtime):
         for StepOne in soup.select('.iusc'):
             link=StepOne.attrs['href']
             url=link
-            
+            time.sleep(0.2)
             time1=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("图片开始保存时间：",time1)
             time1s=time.mktime(time.strptime(time1,"%Y-%m-%d %H:%M:%S"))
@@ -126,7 +175,6 @@ def main(PageNum,InputData,word,overtime):
             count = len(os.listdir('./' + word)) + 1
             overtime=overtime
             SaveImage(link,word,count,overtime)
-            
             time2=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("图片结束保存时间：",time2)
             time2s=time.mktime(time.strptime(time2,"%Y-%m-%d %H:%M:%S"))
@@ -144,8 +192,8 @@ if __name__=='__main__':
     #输入需要加载的页数，每页35幅图像
     PageNum = int(input("请输入要检索的页数，最大不超过20页:"))
     #输入需要搜索的关键字
-    print("超过该时长放弃保存，若保存大图，建议设置15秒以上")
-    overtime=float(input("请设置超时时长:"))
+    #print("超过该时长放弃保存，若保存大图，建议设置15秒以上")
+    overtime=float(input("请设置链接请求超时时长:"))
     word=input("输入需要搜索的关键字:")
     print("-----------------------------------")
     #UTF-8编码
@@ -154,7 +202,7 @@ if __name__=='__main__':
     main(PageNum,InputData,word,overtime)
 
 
-# In[9]:
+# In[ ]:
 
 
 print("-----------------------------------")
