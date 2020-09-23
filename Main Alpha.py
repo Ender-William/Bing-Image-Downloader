792fc35c07766ee7ddd981aaa3f0251cc0870910
William_Kuang <fuwa165@126.com>
2020-09-24 03:42:43 +0800

    Update To 0.2.9
    Add GUI,Easy to use

--- Main Alpha.py
+++ Main Alpha.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
+import PySimpleGUI as sg  
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
@@ -18,128 +19,102 @@ import sys
 from tqdm import tqdm
 import requests#引入requests库
 import time#引入time，计算下载时间
+import tkinter as tk
+import io  
+from PIL import Image, ImageTk 
+import threading
+from tkinter import Tk, Checkbutton, Label
+from tkinter import StringVar, IntVar
+import cv2
+from PIL import Image, ImageTk
 
 
-# In[ ]:
+# In[2]:
 
 
-print("___    __    _________          ")
-print("| |   / /    |  ____  \         ")
-print("| |  / /     | |    \  \        ")
-print("| | / /      | |     \  \       ")
-print("| |/ /       | |     |  |       ")
-print("| |_ \       | |     |  |       ")
-print("| | \ \      | |     |  |       ")
-print("| |  \ \     | |     /  /       ")
-print("| |   \ \    | |____/  /        ")
-print("|_|    \_|   |________/         ")
-print("----------------------------------")
-print("----------------------------------")
-
-
-# In[ ]:
-
-
-#程序声明
-time.sleep(0.1)
-print("本程序从必应图片下载大图")
-time.sleep(0.1)
-print("最多搜索20页的内容")
-time.sleep(0.1)
-print("作者：KDKDKD！")
-time.sleep(0.1)
-print("部分代码来自于开源社区")
-time.sleep(0.1)
-print("版本 Alpha 0.2.8")
-time.sleep(0.1)
-print("时间：2020-09-22")
-time.sleep(0.1)
-print("本软件切勿用于非法用途！")
-time.sleep(0.5)
-print("----------------------------------")
-
-
-# In[ ]:
-
-
-#从得到的图片链接下载图片，并保存
 '''
-#作废的代码，应用于0.2.7及之前的程序里
+通过继承threading.Thread类来避免
+创建的窗口在搜索并下载数据的时候出现
+卡死的情况，不过使用这种方法可能导致
+程序同时执行多个搜索和下载任务，这点
+必须在0.2.9版本之后完成更新来避免此
+类情况。原因是因为程序需要实时显示保
+存情况和保存完的图片的预览。
+'''
+class MyThread(threading.Thread):
+    def __init__(self, func, *args):
+        super().__init__()
+
+        self.func = func
+        self.args = args
+
+        self.setDaemon(True)
+        self.start()  # 在这里开始
+
+    def run(self):
+        self.func(*self.args)
+
+
+# In[3]:
+
+
 def SaveImage(link,InputData,count,overtime):
-        
     try: 
+        print("正在检测网址，请稍后")
+        content = "正在检测网址，请稍后"
+        window.Element('_Multiline_').Update(content)
         socket.setdefaulttimeout(overtime)
         time.sleep(0.1)
-        urllib.request.urlretrieve(link,'./'+InputData+'/'+str(count)+'.jpg')
+        print("正在下载图片，请稍后")
+        content = "正在下载图片，请稍后"
+        window.Element('_Multiline_').Update(content)
+        urllib.request.urlretrieve(link,'./'+InputData+'/'+str(count)+'.png')
     except socket.timeout:
         time.sleep(0.1)
         print("链接超时")
+        content ="链接超时"
+        
+        window.Element('_Multiline_').Update(content)
     except urllib.error.HTTPError as reason:
         print(reason)
+        content =reason
+        window.Element('_Multiline_').Update(content)
     except Exception:
         time.sleep(0.1)
         print("无效链接")
+        content ="无效链接"
+        window.Element('_Multiline_').Update(content)
     else:
         print("已有" + str(count) + "张图")
-'''
-
-
-# In[ ]:
-
-
-def SaveImage(link,InputData,count,overtime):
-    
-    try:
-        socket.setdefaulttimeout(overtime)
+        content = str(count)
+        window.Element('Picture_Num').Update(content)
         time.sleep(0.1)
-        urllib.request.urlretrieve(link,'./'+InputData+'/'+str(count)+'.jpg')
-        print("正在检测网址，请稍后")
-        start = time.time()
-        size = 0
-        path = './'+InputData+'/'+str(count)+'.jpg' #必须是完整路径
-        response = requests.get(link,stream = True)#stream参数设置成True时，它不会立即开始下载，当你使用iter_content或iter_lines遍历内容或访问内容属性时才开始下载
-        chunk_size = 1024#每次块大小为1024
-        content_size = int(response.headers['content-length'])#返回的response的headers中获取文件大小信息
-        print("文件大小："+str(round(float(content_size/chunk_size/1024),4))+"[MiB]")
-        with open(path,'wb') as file:
-            for data in response.iter_content(chunk_size=chunk_size):#每次只获取一个chunk_size大小
-                file.write(data)#每次只写入data大小
-                size = len(data)+size
-                #'r'每次重新从开始输出，end = ""是不换行
-                print('\r'+"【"+str(round(float(size/content_size)*100,2))+"%"+"】"+"已经下载："+int(size/content_size*30)*"█"+" 【"+str(round(size/chunk_size/1024,2))+"MiB】",end="")
-        end = time.time()
-        print("")
-        print("保存图片耗时耗时:"+str(end-start)+"秒")
-        #片段的程序来源：https://blog.csdn.net/YiXiao1997/article/details/86721536?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param
         
-        print("已有" + str(count) + "张图")
-    except socket.timeout:
-        time.sleep(0.1)
-        print("链接超时")
-    except urllib.error.HTTPError as reason:
-        print(reason)
-    except Exception:
-        time.sleep(0.1)
-        print("无效链接")
-    #else:
-        #print("已有" + str(count) + "张图")
+        Path ='./'+InputData+'/'+str(count)+'.png'
+        picture_type = 'png'
+        newpath = InputData
+        if not os.path.exists(newpath):
+            os.mkdir(newpath)
+        
+        #portion = os.path.splitext(Path)
+        #print('convert  ' + newpath +'  to '+portion[0]+'.'+picture_type)
+        img = cv2.imread(Path)
+        cv2.imwrite("./"+newpath+"/"+str(count)+'.'+picture_type,img)
+        #photo = ImageTk.PhotoImage(file=Path)
+        NPATH = "./"+newpath+"/"+str(count)+'.'+picture_type
+        window.Element('Image').Update(NPATH,size=(1100, 1100),visible=True)
 
 
-# In[ ]:
+# In[4]:
 
 
 # 主函数
 def main(PageNum,InputData,word,overtime):
     for i in range(PageNum):
-        print("-----------------------------------")
-        print("-----------------------------------")
-        print("-----------------------------------")
-        print("-----------------------------------")
         print("正在检索的页数：",i+1)
-        print("-----------------------------------")
-        print("-----------------------------------")
-        print("-----------------------------------")
-        print("-----------------------------------")
+        NUM =i+1
+        content = "正在检索的页数："+ str(NUM)
+        window.Element('_Multiline_').Update(content)
         #try:
         url = 'http://cn.bing.com/images/async?q={0}&first={1}&count=35&relp=35&lostate=r&mmasync=1&dgState=x*175_y*848_h*199_c*1_i*106_r*0'
         #定义请求头
@@ -153,11 +128,14 @@ def main(PageNum,InputData,word,overtime):
         if not os.path.exists("./" + word):
             os.mkdir('./' + word)
         for StepOne in soup.select('.iusc'):
+            
             link=StepOne.attrs['href']
             url=link
             time.sleep(0.2)
             time1=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("图片开始保存时间：",time1)
+            content ="图片开始保存时间："+ time1
+            window.Element('_Multiline_').Update(content)
             time1s=time.mktime(time.strptime(time1,"%Y-%m-%d %H:%M:%S"))
             #print("原始访问链接：",url)
             
@@ -170,6 +148,8 @@ def main(PageNum,InputData,word,overtime):
             #print("list",otherURL)
             imageURL="http://"+otherURL+".jpg"
             print("图像链接：",imageURL)
+            content = "图像链接："+ imageURL
+            window.Element('_Multiline_').Update(content)
             link=imageURL
             time.sleep(1)
             count = len(os.listdir('./' + word)) + 1
@@ -177,36 +157,72 @@ def main(PageNum,InputData,word,overtime):
             SaveImage(link,word,count,overtime)
             time2=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("图片结束保存时间：",time2)
+            content = "图片结束保存时间：" + time2
+            window.Element('_Multiline_').Update(content)
             time2s=time.mktime(time.strptime(time2,"%Y-%m-%d %H:%M:%S"))
             delta=time2s-time1s
             print("保存耗时：",delta)
+            content = "保存耗时：" + str(delta)
+            window.Element('_Multiline_').Update(content)
             print("-----------------------------------")
         #except:
             #print('URL OPENING ERROR !')
+    content = "完成下载"
+    window.Element('_Multiline_').Update(content)
 
 
-# In[ ]:
+# In[5]:
 
 
-if __name__=='__main__':
-    #输入需要加载的页数，每页35幅图像
-    PageNum = int(input("请输入要检索的页数，最大不超过20页:"))
-    #输入需要搜索的关键字
-    #print("超过该时长放弃保存，若保存大图，建议设置15秒以上")
-    overtime=float(input("请设置链接请求超时时长:"))
-    word=input("输入需要搜索的关键字:")
-    print("-----------------------------------")
-    #UTF-8编码
+def Start_Search ():
+    PageNum = value['_PageNum_']
+    PageNum = int(PageNum)
+    overtime = value['_OverTime_']
+    overtime = float(overtime)
+    word = value['_KeyWords_']
     InputData=urllib.parse.quote(word)
-    #print(InputData)
     main(PageNum,InputData,word,overtime)
 
 
-# In[ ]:
+# In[7]:
 
 
-print("-----------------------------------")
-input("按下回车键退出程序")
+import PySimpleGUI as sg
+
+
+# 创建布局
+layout = [
+    [sg.Text('请输入页数',font=(30), justification='center'), sg.Input(key='_PageNum_',size = (50,1))],
+    [sg.Text('请输入时长',font=(30), justification='center'), sg.Input(key='_OverTime_',size = (50,1))],
+    [sg.Text('输入关键字',font=(30), justification='center'), sg.Input(key='_KeyWords_',size = (50,1))],
+    [sg.Btn('开始检索', key='_START_',font=(30)),sg.Btn('终止检索', key='_END_',font=(30))],
+    [sg.Text('已有图片张数：',font=(30), justification='center'),
+     sg.Text('0',key ='Picture_Num',size = (5,1),font=(30), justification='center')],
+    [sg.Multiline('还未开始检索', key = '_Multiline_',size = (40,10),autoscroll = True ,font=(30)),
+     sg.Image(size=(1100,1100),filename=r"logo.jpg",key = 'Image',visible=True)],
+    #[sg.Image(size=(1100,1100),
+           #   filename=r"logo.jpg",key = 'Image')]
+]
+
+
+# 创建窗口，引入布局，并进行初始化
+# 创建时，必须要有一个名称，这个名称会显示在窗口上
+window = sg.Window('Picture search and downloader Alpha 0.2.9', layout=layout, finalize=True)
+
+while True:  # 创建一个事件循环，否则窗口运行一次就会被关闭
+    event, value = window.Read()  # event, value 的值分别是 _LOGIN_ {'_USER_': 'admin', '_PWD_': '123'}
+    # print(event, value)  # 可以打印一下看看变量的内容
+    if event is None:   # 如果事件的值为 None，表示点击了右上角的关闭按钮
+        break
+    if event == '_END_':   # 
+        break
+    if event == '_START_':  # 当获取到事件时，处理逻辑
+        #print(InputData)
+        MyThread(Start_Search)
+        
+
+
+window.close()
 
 
 # In[ ]:
