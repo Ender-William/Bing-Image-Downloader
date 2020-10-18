a7fae4475593b34c7deff8ec94571d1704296535
Ender-William <39450310+Ender-William@users.noreply.github.com>
2020-10-18 13:52:29 +0800

    Update to 0.3.0

--- Image Search/Image Search.py
+++ Image Search/Image Search.py
@@ -1,35 +1,102 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
-import PySimpleGUI as sg  
-import urllib.request
-import urllib.parse
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import os
-import time
-import requests
-from lxml import etree
-import re
-import socket
-import sys
-from tqdm import tqdm
-import requests#引入requests库
 import time#引入time，计算下载时间
-import tkinter as tk
+
+
+# In[ ]:
+
+
+print("___    __    _________          ")
+print("| |   / /    |  ____  \         ")
+print("| |  / /     | |    \  \        ")
+print("| | / /      | |     \  \       ")
+print("| |/ /       | |     |  |       ")
+print("| |_ \       | |     |  |       ")
+print("| | \ \      | |     |  |       ")
+print("| |  \ \     | |     /  /       ")
+print("| |   \ \    | |____/  /        ")
+print("|_|    \_|   |________/         ")
+print("----------------------------------")
+#程序声明
+time.sleep(0.1)
+print("本程序从必应图片下载大图")
+time.sleep(0.1)
+print("最多搜索20页的内容")
+time.sleep(0.1)
+print("作者：KDKDKD！")
+time.sleep(0.1)
+print("部分代码来自于开源社区")
+time.sleep(0.1)
+print("版本 Alpha 0.3.0")
+time.sleep(0.1)
+print("时间：2020-10-18")
+time.sleep(0.1)
+print("本软件切勿用于非法用途！")
+time.sleep(0.1)
+print("程序正在加载，请稍等")
+time.sleep(0.5)
+print("----------------------------------")
+
+
+# In[ ]:
+
+
+print("import PySimpleGUI as sg")
+import PySimpleGUI as sg
+print("import urllib.request")
+import urllib.request
+print("import urllib.parse")
+import urllib.parse
+print("from bs4 import BeautifulSoup")
+from bs4 import BeautifulSoup
+print("from urllib.request import urlopen")
+from urllib.request import urlopen
+print("import os")
+import os
+print("from lxml import etree")
+from lxml import etree
+print("import re")
+import re
+print("import socket")
+import socket
+print("import sys")
+import sys
+print("from tqdm import tqdm")
+from tqdm import tqdm
+print("import requests")
+import requests#引入requests库
+print("import io  ")
 import io  
+print("from PIL import Image, ImageTk ")
 from PIL import Image, ImageTk 
+print("import threading")
 import threading
+print("from tkinter import Tk, Checkbutton, Label")
 from tkinter import Tk, Checkbutton, Label
+print("from tkinter import StringVar, IntVar")
 from tkinter import StringVar, IntVar
+print("import cv2")
 import cv2
-from PIL import Image, ImageTk
+print("import inspect")
+import inspect
+print("import ctypes")
+import ctypes
+print("import itertools")
+import itertools
+print("import _thread")
+import _thread
+
+time.sleep(0.5)
+print("----------------------------------")
+print("Python库加载完毕")
+print("----------------------------------")
 
 
-# In[2]:
+# In[ ]:
 
 
 '''
@@ -37,10 +104,11 @@ from PIL import Image, ImageTk
 创建的窗口在搜索并下载数据的时候出现
 卡死的情况，不过使用这种方法可能导致
 程序同时执行多个搜索和下载任务，这点
-必须在0.2.9版本之后完成更新来避免此
+必须在0.2.8版本之后完成更新来避免此
 类情况。原因是因为程序需要实时显示保
 存情况和保存完的图片的预览。
 '''
+
 class MyThread(threading.Thread):
     def __init__(self, func, *args):
         super().__init__()
@@ -53,28 +121,100 @@ class MyThread(threading.Thread):
 
     def run(self):
         self.func(*self.args)
+        
 
 
-# In[3]:
+# In[ ]:
+
+
+
+'''
+此段程序是为了完成线程启动、暂停、重启
+以及线程终止的，方便用户在搜索完一个类
+型的照片或者终止检索一种类型的照片之后
+不必重启程序，从而可以继续搜索其他照片
+而使用.
+
+class MyThread(threading.Thread):
+    def __init__(self, func, *args):
+        super().__init__()
+
+        self.func = func
+        self.args = args
+
+        self.setDaemon(True)
+        self.start()  # 在这里开始
+
+    def run(self):
+        self.func(*self.args)      # 为True时立即返回, 为False时阻塞直到内部的标识位为True后返回
+            
+
+    def pause(self):
+        self.__flag.clear()     # 设置为False, 让线程阻塞
+
+    def resume(self):
+        self.__flag.set()    # 设置为True, 让线程停止阻塞
+
+    def stop(self):
+        self.__flag.set()       # 将线程从暂停状态恢复, 如何已经暂停的话
+        self.__running.clear()        # 设置为False    
+'''
+
+
+# In[ ]:
+
+
+'''
+这段程序是用作终止子线程而使用的，
+避免在终止搜索图片之后重新打开一次
+程序，减少了时间上的浪费
+'''
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+ 
+ 
+def stop_thread(thread):
+    _async_raise(thread.ident, SystemExit)
+
+
+# In[ ]:
 
 
 def SaveImage(link,InputData,count,overtime):
     try: 
+        '''
+        这段程序向目标网址发出链接请求
+        并检测反馈时间和反馈类型
+        '''
         print("正在检测网址，请稍后")
         content = "正在检测网址，请稍后"
         window.Element('_Multiline_').Update(content)
-        socket.setdefaulttimeout(overtime)
+        socket.setdefaulttimeout(overtime) #设定访问超时时长
         time.sleep(0.1)
+        '''
+        假设链接可以访问，则开始下载并保存图片
+        '''
         print("正在下载图片，请稍后")
         content = "正在下载图片，请稍后"
         window.Element('_Multiline_').Update(content)
         urllib.request.urlretrieve(link,'./'+InputData+'/'+str(count)+'.jpg')
         urllib.request.urlretrieve(link,'./'+"Preview"+'/'+str(count)+'.png')
     except socket.timeout:
+        #链接超时，抛出异常
         time.sleep(0.1)
         print("链接超时")
         content ="链接超时"
-        
         window.Element('_Multiline_').Update(content)
     except urllib.error.HTTPError as reason:
         print(reason)
@@ -91,26 +231,72 @@ def SaveImage(link,InputData,count,overtime):
         window.Element('Picture_Num').Update(content)
         time.sleep(0.1)
         try:
+            '''
+            使用OpenCV模组读取jpg格式的图片
+            并将图片转化为png格式的图片后保存
+            至Preview文件夹内。Preview文件
+            夹内的图片是为了图形化界面预览使用
+            '''
             Path ='./'+"Preview"+'/'+str(count)+'.png'
             picture_type = 'png'
             newpath = 'Preview'
-            if not os.path.exists(newpath):
-                os.mkdir(newpath)
-
+            
+            
             #portion = os.path.splitext(Path)
             #print('convert  ' + newpath +'  to '+portion[0]+'.'+picture_type)
             img = cv2.imread(Path)
-            cv2.imwrite("./"+newpath+"/"+str(count)+'.'+picture_type,img)
-            #photo = ImageTk.PhotoImage(file=Path)
             NPATH = "./"+newpath+"/"+str(count)+'.'+picture_type
-            window.Element('Image').Update(NPATH,size=(1100, 1100),visible=True)
+
+            '''
+            这段程序将使图片适应软件窗口500x500的大小
+            '''
+            x, y = img.shape[0:2] # 获取图片的宽与高 x y
+ 
+            if x < y:
+                Scale = y // 500
+                if y <=1:
+                    SC = y/500
+                    img = cv2.resize(img,dsize=None,fx=SC,fy=SC, interpolation=cv2.INTER_NEAREST)
+                    cv2.imwrite(NPATH,img)
+                else:
+                    SC = 500/y
+                    img = cv2.resize(img,dsize=None,fx=SC,fy=SC,interpolation=cv2.INTER_NEAREST)
+                    cv2.imwrite(NPATH,img)
+
+            else:
+                Scale = x // 500
+                if x <=1:
+                    SC = x/500
+                    img = cv2.resize(img,dsize=None,fx=SC,fy=SC,interpolation=cv2.INTER_NEAREST)
+                    cv2.imwrite(NPATH,img)
+                else:
+                    SC = 500/x
+                    img = cv2.resize(img,dsize=None,fx=SC,fy=SC,interpolation=cv2.INTER_NEAREST)
+                    cv2.imwrite(NPATH,img)
+
+
+
+
+            #img = cv2.resize(img,(500,500))
+            #cv2.imwrite(NPATH,img)
+            img = cv2.imread(NPATH)
+            cv2.imwrite("./"+newpath+"/"+str(count)+'.'+picture_type,img)
+
+            #image = cv2.imread(NPATH)
+            #image = cv2.resize(image,(500),interpolation=cv2.INTER_AREA)
+            #cv2.imwrite("./"+newpath+"/"+str(count)+'.'+picture_type,img)
+            #photo = ImageTk.PhotoImage(file=Path)
+
+
+            window.Element('Image').Update(NPATH,size=(500, 500),visible=True)
+
         except:
             content = 'CV2 模组错误，无法预览图片'
             window.Element('_Multiline_').Update(content)
             time.sleep(0.1)
 
 
-# In[4]:
+# In[ ]:
 
 
 # 主函数
@@ -158,7 +344,7 @@ def main(PageNum,InputData,word,overtime):
             content = "图像链接："+ imageURL
             window.Element('_Multiline_').Update(content)
             link=imageURL
-            time.sleep(1)
+            time.sleep(0.1)
             count = len(os.listdir('./' + word)) + 1
             overtime=overtime
             SaveImage(link,word,count,overtime)
@@ -178,7 +364,7 @@ def main(PageNum,InputData,word,overtime):
     window.Element('_Multiline_').Update(content)
 
 
-# In[5]:
+# In[ ]:
 
 
 def Start_Search ():
@@ -191,10 +377,7 @@ def Start_Search ():
     main(PageNum,InputData,word,overtime)
 
 
-# In[6]:
-
-
-import PySimpleGUI as sg
+# In[ ]:
 
 
 # 创建布局
@@ -202,31 +385,55 @@ layout = [
     [sg.Text('请输入页数',font=(30), justification='center'), sg.Input(key='_PageNum_',size = (50,1))],
     [sg.Text('请输入时长',font=(30), justification='center'), sg.Input(key='_OverTime_',size = (50,1))],
     [sg.Text('输入关键字',font=(30), justification='center'), sg.Input(key='_KeyWords_',size = (50,1))],
-    [sg.Btn('开始检索', key='_START_',font=(30)),sg.Btn('终止检索', key='_END_',font=(30))],
+    [sg.Btn('开始检索', key='_START_',font=(30)),sg.Btn('终止检索', key='_STOP_',font=(30),disabled=True)],
     [sg.Text('已有图片张数：',font=(30), justification='center'),
      sg.Text('0',key ='Picture_Num',size = (5,1),font=(30), justification='center')],
     [sg.Multiline('还未开始检索', key = '_Multiline_',size = (40,10),autoscroll = True ,font=(30)),
-     sg.Image(size=(1100,1100),filename=r"logo.jpg",key = 'Image',visible=True)],
-    #[sg.Image(size=(1100,1100),
-           #   filename=r"logo.jpg",key = 'Image')]
+     sg.Image(size=(500,500),filename=r"logo.png",key = 'Image',visible=True)],
 ]
 
 
 # 创建窗口，引入布局，并进行初始化
 # 创建时，必须要有一个名称，这个名称会显示在窗口上
-window = sg.Window('Picture search and downloader Alpha 0.2.9', layout=layout, finalize=True)
+window = sg.Window('Picture search and downloader Alpha 0.3.0', layout=layout, finalize=True)
 
+
+#Start_Search = MyThread
+threading = threading
+MyThread = threading.Thread(target=Start_Search)
 while True:  # 创建一个事件循环，否则窗口运行一次就会被关闭
+    
     event, value = window.Read()  # event, value 的值分别是 _LOGIN_ {'_USER_': 'admin', '_PWD_': '123'}
     # print(event, value)  # 可以打印一下看看变量的内容
-    if event is None:   # 如果事件的值为 None，表示点击了右上角的关闭按钮
+    if event is None:   # 如果事件的值为 None，表示点击了左上角的关闭按钮
         break
-    if event == '_END_':   # 
-        break
-    if event == '_START_':  # 当获取到事件时，处理逻辑
-        #print(InputData)
-        MyThread(Start_Search)
+    if event == '_STOP_':   # 
+        window.Element('_STOP_').Update(disabled = True)
+        content = "正在终止检索"
+        window.Element('_Multiline_').Update(content)
+        time.sleep(1)
+        #break
+        stop_thread(MyThread)
+        #MyThread.pause
+        MyThread.join()
+        content = "正在回收线程"
+        window.Element('_Multiline_').Update(content)
+        time.sleep(1)
+        #window.Element('_STOP_').Update(disabled = True)
+        content = "已经终止检索"
+        window.Element('_Multiline_').Update(content)
         
+    if event == '_START_':  # 当获取到事件时，处理逻辑
+        MyThread = threading.Thread(target=Start_Search)
+        MyThread.start()
+
+
+        #print(InputData)
+        #MyThread(Start_Search)
+        #Start_Search = MyThread
+        #Start_Search.start(Start_Search)
+        
+        window.Element('_STOP_').Update(disabled = False)
 
 
 window.close()
